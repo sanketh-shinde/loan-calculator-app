c12304b8b96eabda38a87cb6836bf6b52f2711ba
Sanketh Shinde <sankethshinde99@gmail.com>
2023-12-31 17:50:56 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const amount = document.getElementById('amount');
+const interestRate = document.getElementById('interest-rate');
+const months = document.getElementById('months');
+const calculateButton = document.getElementById('btn');
+
+calculateButton.addEventListener('click', () => {
+
+  const rateOfInterestPerAnnum = interestRate.value / 12 / 100;
+
+  const emi = Math.round((amount.value * rateOfInterestPerAnnum * (Math.pow(1 + rateOfInterestPerAnnum, months.value))) / (Math.pow(1 + rateOfInterestPerAnnum, months.value) - 1))
+
+  const totalInterestPayable = Math.round(emi * months.value - amount.value);
+
+  document.getElementById('display').innerHTML = `<hr><p>your total emi is: ${emi}</p> <p>your total interset payable is: ${totalInterestPayable}</p><p>your total amount payable is: ${emi * months.value}</p>`;
+});
